79034b4d2d1b2f705799b84885f960142b148064
Jan Malakhovski <oxij@oxij.org>
2015-08-17 23:55:04 +0000

    haskell-packages: make hoogle wrapper much more helpful
    * Build most of the stuff on /tmp, not in /nix/store.
    * Generate hoogle database for all the dependencies.
    * Generate haddock index and contents files.
    * Cleanup.

--- pkgs/development/haskell-modules/lib.nix
+++ pkgs/development/haskell-modules/lib.nix
@@ -82,6 +82,11 @@ rec {
 
   triggerRebuild = drv: i: overrideCabal drv (drv: { postUnpack = ": trigger rebuild ${toString i}"; });
 
-  #FIXME: throw this away sometime in the future. added 2015-08-18
-  withHoogle = throw "withHoogle is no longer supported, use ghcWithHoogle instead";
+  withHoogle = haskellEnv: with haskellEnv.haskellPackages;
+    import ./hoogle.nix {
+      inherit (pkgs) stdenv;
+      inherit (pkgs.stdenv) lib;
+      inherit hoogle rehoo ghc;
+      packages = haskellEnv.paths;
+    };
 }
