36ef334b9adac52ff38015278f537ec21d9885a1
Bjørn Forsman <bjorn.forsman@gmail.com>
2015-08-17 19:12:30 +0200

    freeglut: remove unused patch

--- pkgs/development/libraries/freeglut/default.nix
+++ pkgs/development/libraries/freeglut/default.nix
@@ -11,19 +11,11 @@ in stdenv.mkDerivation {
 
   buildInputs = [ libXi libXrandr libXxf86vm mesa x11 cmake ];
 
-  meta = with stdenv.lib; {
-    description = "Create and manage windows containing OpenGL contexts";
-    longDescription = ''
-      FreeGLUT is an open source alternative to the OpenGL Utility Toolkit
-      (GLUT) library. GLUT (and hence FreeGLUT) allows the user to create and
-      manage windows containing OpenGL contexts on a wide range of platforms
-      and also read the mouse, keyboard and joystick functions. FreeGLUT is
-      intended to be a full replacement for GLUT, and has only a few
-      differences.
-    '';
-    homepage = http://freeglut.sourceforge.net/;
-    license = licenses.mit;
-    platforms = platforms.all;
-    maintainers = [ maintainers.bjornfor ];
-  };
+  postPatch = lib.optionalString stdenv.isDarwin ''
+    substituteInPlace Makefile.am --replace \
+      "SUBDIRS = src include progs doc" \
+      "SUBDIRS = src include doc"
+  '';
+
+  configureFlags = [ "--enable-warnings" ];
 }
