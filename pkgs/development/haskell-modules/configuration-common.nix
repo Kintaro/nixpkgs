5c909289515081e6795ee3a05a735f5abc4ef9b9
Peter Simons <simons@cryp.to>
2015-08-17 22:27:17 +0200

    haskell-shelly has transient test suite failures.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -965,7 +965,4 @@ self: super: {
   # https://github.com/yesodweb/Shelly.hs/issues/108
   shelly = dontCheck super.shelly;
 
-  # https://github.com/bos/configurator/issues/22
-  configurator = dontCheck super.configurator;
-
 }
