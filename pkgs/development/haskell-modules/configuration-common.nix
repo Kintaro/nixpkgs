2739610e982b715c5af8cc73c748fab0f04c0dde
Peter Simons <simons@cryp.to>
2015-08-18 22:54:42 +0200

    haskell-configurator fails its test suite.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -961,4 +961,7 @@ self: super: {
   # https://github.com/yesodweb/Shelly.hs/issues/108
   shelly = dontCheck super.shelly;
 
+  # https://github.com/bos/configurator/issues/22
+  configurator = dontCheck super.configurator;
+
 }
