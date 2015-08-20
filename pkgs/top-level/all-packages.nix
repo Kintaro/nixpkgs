67b692f4ea9fc79a8540a786acec795d85b17d34
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-08-20 11:49:55 +0200

    Revert gcc darwin fixes
    This reverts commits cb7c053383ff76a454484992b3a6d6a2f1530c15,
    ebda45e6b9db3225c30a998b2624d90b0c08edb4,
    4a495cfbab2b3ab9ae8219ef1812c766bf27b01e because they broke Nixpkgs
    evaluation.
    
    http://hydra.nixos.org/build/24813270

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3887,8 +3887,6 @@ let
     libcCross = if crossSystem != null then libcCross else null;
 
     isl = isl_0_14;
-
-    inherit (darwin) CF;
   }));
 
   gcc49 = lowPrio (wrapCC (callPackage ../development/compilers/gcc/4.9 {
@@ -3906,8 +3904,6 @@ let
     isl = isl_0_11;
 
     cloog = cloog_0_18_0;
-
-    inherit (darwin) CF;
   }));
 
   gcc5 = lowPrio (wrapCC (callPackage ../development/compilers/gcc/5 {
@@ -3923,8 +3919,6 @@ let
     libcCross = if crossSystem != null then libcCross else null;
 
     isl = isl_0_14;
-
-    inherit (darwin) CF;
   }));
 
   gfortran = if !stdenv.isDarwin then gfortran49
