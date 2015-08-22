304c4e1775e6a50be563bfe38274dbf4402c890a
Nikolay Amiantov <ab@fmap.me>
2015-08-22 12:25:18 +0300

    yed: 3.14.2 -> 3.14.3, mark as not redistributable
    Their license explicitly prohibits distribution and it's unclear whether they allow to download their binary automatically. I've asked to clarify their terms; this may be temporary.

--- pkgs/applications/graphics/yed/default.nix
+++ pkgs/applications/graphics/yed/default.nix
@@ -3,9 +3,14 @@
 stdenv.mkDerivation rec {
   name = "yEd-3.14.3";
 
-  src = fetchurl {
-    url = "http://www.yworks.com/products/yed/demo/${name}.zip";
-    sha256 = "0i39n8h97v688r0nydcm0wj1ins2v83xjgykr7czpnmp600cq9cy";
+  #src = fetchurl {
+  #  url = "http://www.yworks.com/products/yed/demo/${name}.zip";
+  #  sha256 = "0xgazknbz82sgk65hxmvbycl1vd25z80a7jgwjgw7syicrgmplcl";
+  #};
+  src = requireFile {
+    name = "${name}.zip";
+    url = "https://www.yworks.com/en/products_download.php?file=${name}.zip";
+    sha256 = "0xgazknbz82sgk65hxmvbycl1vd25z80a7jgwjgw7syicrgmplcl";
   };
 
   nativeBuildInputs = [ unzip makeWrapper ];
